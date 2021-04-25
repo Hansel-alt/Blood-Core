433ba213d274e7fcdf6151d054d140bb6aadfc56
Hansel-alt <60404509+Hansel-alt@users.noreply.github.com>
2021-04-25 03:36:30 -0400

    Add files via upload

---
+++ Project/App/forms.py
@@ -0,0 +1,22 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, TextAreaField, SubmitField
+from wtforms.validators import InputRequired, EqualTo, Email, Length
+from wtforms.fields.html5 import EmailField, DateField
+from email_validator import validate_email
+from datetime import datetime
+
+class SignUp(FlaskForm):
+    firstname = StringField('First Name', validators=[InputRequired()])
+    lastname = StringField('Last Name', validators=[InputRequired()])
+    dob = DateField('Date of Birth', format='%d/%m/%Y', validators=[InputRequired()])
+    address = StringField('Address', validators=[InputRequired()])
+    username = StringField('Username', validators=[InputRequired(), Length(min=3, max=15)])
+    email = EmailField('Email', validators=[Email(), InputRequired()])
+    password = PasswordField('New Password', validators=[InputRequired(), Length(min=7, max=80), EqualTo('confirm', message='Passwords must match')])
+    confirm  = PasswordField('Repeat Password')
+    submit = SubmitField('Sign Up', render_kw={'class': 'btn w3-indigo waves-effect waves-red white-text'})
+
+class LogIn(FlaskForm):
+    username = StringField('Username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=80)])
+    submit = SubmitField('Login', render_kw={'class': 'btn w3-indigo waves-effect waves-red white-text'})
---
+++ Project/App/models.py
@@ -0,0 +1,81 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+from forms import SignUp, LogIn
+from datetime import datetime
+db = SQLAlchemy()
+
+## Create a User Model
+class User(UserMixin, db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String(80), unique=True, nullable=False)
+  email = db.Column(db.String(120), unique=True, nullable=False)
+  password = db.Column(db.String(120), nullable=False)
+  firstname = db.Column(db.String(120))
+  lastname = db.Column(db.String(120))
+  address = db.Column(db.String(200))
+  dateofbirth = db.Column(db.DateTime)
+  
+  def toDict(self):
+    return {
+      "id": self.id,
+      "firstname": self.firstname,
+      "lastname": self.lastname,
+      "username": self.username,
+      "email": self.email,
+      "password":self.password,
+      "address": self.address,
+      'dateofbirth': self.dateofbirth.strftime("%d/%m/%Y, %H:%M:%S")
+    }
+    
+  #hashes the password parameter and stores it in the object
+  def set_password(self, password):
+    """Create hashed password."""
+    self.password = generate_password_hash(password, method='sha256')
+    
+  #Returns true if the parameter is equal to the object's password property
+  def check_password(self, password):
+    """Check hashed password."""
+    return check_password_hash(self.password, password)
+    
+  #To String method
+  def __repr__(self):
+    return '<User {}>'.format(self.username) 
+
+## Create a Medical Model
+class Medical(db.Model):
+  medid = db.Column('medid', db.Integer, primary_key=True)
+  height = db.Column(db.Integer)
+  weight = db.Column(db.Integer)
+  bmi = db.Column(db.String(100))
+  bst_type = db.Column(db.String(100))
+  bs_test = db.Column(db.Integer)
+  blood_sugar = db.Column(db.String(100))
+  bp_systolic = db.Column(db.Integer)
+  bp_diastolic = db.Column(db.Integer)
+  bp_pulse = db.Column(db.Integer)
+  blood_pressure = db.Column(db.String(100))
+  checkup = db.Column(db.DateTime, default=datetime.utcnow)
+
+  def toDict(self):
+    return{
+      'medid': self.medid,
+      'checkup': self.checkup.strftime("%d/%m/%Y"),
+      'body': {
+          'height': self.height,
+          'weight': self.weight,
+          'bmi': self.bmi
+          },
+      'blood_sugar': {
+          'type': self.bst_type,
+          'test': self.bs_test,
+          'bs_result': self.blood_sugar
+          },
+      'blood_pressure': {
+          'bp_systolic': self.bp_systolic,
+          'bp_diastolic': self.bp_diastolic,
+          'bp_pulse': self.bp_pulse,
+          'bs_result': self.blood_pressure
+          },
+    }
+
