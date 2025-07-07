991b08984463826720e6e7c377f64b9e6b65b16d
Hansel <hansel.charles@gmail.com>
2025-07-07 14:27:23 -0400

    Initial commit for Render deploy

--- main.py
+++ main.py
@@ -165,4 +165,5 @@ def list_users():
     return json.dumps(users)
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=8080, debug=True)
+  #app.run(host='0.0.0.0', port=8080, debug=True)
+  app.run(host='0.0.0.0', port=10000)
